450b1ff3897c87a8916724d9ec473306fbd31b46
syedhassani <syedmhassani12@gmail.com>
2024-09-25 19:17:34 +1000

    python operators and datatypes

---
+++ main .py
@@ -0,0 +1,17 @@
+# #activity
+# name="jim"
+# print(name)
+# print(type(name))
+# age=10.30
+# print(age)
+# print(type(age))
+# activity 2
+num1=10
+num2=9000
+print("addtion of two numbers",num1+num2)
+num1=89
+num2=31
+print("sub of two numbers",num1-num2)
+num1=56
+num2=28
+print("product of two numbers",num1*num2)
